c3765b2054eb1aabe061b31012827cb917ee6cd8
Mat-Frayne <mat.frayne@gmail.com>
2019-09-02 18:24:12 +0930

    .

--- Javascript/insert_Comma_Into_Int.js
+++
@@ -1,3 +0,0 @@
-function toCommas(value) {
-    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
--- Python/reverse.py
+++
@@ -1,3 +0,0 @@
-def reverseString(string):
-    """Reverse the input String."""
-    return string[::-1]
